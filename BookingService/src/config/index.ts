a951a9cd7dab881b446a723e3d0197837d0f8ae7
Aditya Gaur <aditya@krazimo.com>
2026-05-10 12:23:53 +0530

    add distributed cache lock with redis redlock

--- BookingService/src/config/index.ts
+++ BookingService/src/config/index.ts
@@ -3,6 +3,8 @@ import dotenv from 'dotenv';
 
 type ServerConfig = {
     PORT: number
+    REDIS_SERVER_URL:string
+    LOCK_TTL:number
 }
 
 function loadEnv() {
@@ -13,5 +15,7 @@ function loadEnv() {
 loadEnv();
 
 export const serverConfig: ServerConfig = {
-    PORT: Number(process.env.PORT) || 3001
+    PORT: Number(process.env.PORT) || 3001,
+    REDIS_SERVER_URL:(process.env.REDIS_SERVER_URL )|| 'redis://localhost:6379',
+    LOCK_TTL:Number(process.env.LOCK_TTL)||60000
 };
